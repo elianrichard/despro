07b8e590a9680435b9d65c266c7768bb4a4fa6a7
elianrichard <elianrichards@gmail.com>
2022-11-11 01:21:39 +0700

    initial setup

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex w-screen h-screen bg-gray-900">
+      <h1 className="text-white">Hello World!!!</h1>
+    </div>
+  );
+};
+
+export default Home;
